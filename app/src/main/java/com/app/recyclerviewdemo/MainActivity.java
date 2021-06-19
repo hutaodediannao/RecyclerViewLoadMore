09cc846c65e44cc8a5399bf41c033b327b108581
hutaoCode <643759269@qq.com>
2021-06-19 17:24:50 +0800

    1、添加了isLoading字段控制上拉到底部，触发加载，防止重复加载导致的bug，致使列表混乱的bug

--- app/src/main/java/com/app/recyclerviewdemo/MainActivity.java
+++ app/src/main/java/com/app/recyclerviewdemo/MainActivity.java
@@ -1,11 +1,13 @@
 package com.app.recyclerviewdemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -23,6 +25,7 @@ import java.util.List;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     private List<Model> list;
     private RecyclerView recyclerView;
     private ModelAdapter adapter;
@@ -52,13 +55,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 list.clear();
-                for (int i = 0; i < 3; i++) {
+                for (int i = 0; i < 5; i++) {
                     list.add(new Model("普通数据: " + i, BaseRecyclerAdapter.NORMAL_TYPE));
                 }
                 adapter.notifyDataSetChanged();
                 currentPageIndex = 1;
             }
         }, 2000);
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    LinearLayoutManager manager = (LinearLayoutManager) recyclerView.getLayoutManager();
+                    int lastPos = manager.findLastVisibleItemPosition();
+
+                    if (manager.getChildCount() > 0 && lastPos > manager.getItemCount() - 2
+                            && manager.getItemCount() > manager.getChildCount()) {
+                        Log.i(TAG, "lastPos ======= 开始加载数据 =========> " + lastPos);
+                        if (!isLoading) {
+                            loadMoreData();
+                        }
+                    }
+                }
+            }
+        });
     }
 
     private void refreshData() {
@@ -73,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
                 //一出加载进度条
                 list.remove(list.size() - 1);
 
-                for (int i = 0; i < 3; i++) {
+                for (int i = 0; i < 5; i++) {
                     list.add(new Model("普通数据:" + i, BaseRecyclerAdapter.NORMAL_TYPE));
                 }
                 adapter.notifyDataSetChanged();
@@ -86,14 +108,20 @@ public class MainActivity extends AppCompatActivity {
         refreshData();
     }
 
+    //限制列表在加载状态下，是无法触发再次加载的
+    private boolean isLoading = false;
+
     private void loadMoreData() {
-        if (currentPageIndex == maxPageTotal) {
+        if (currentPageIndex >= maxPageTotal) {
             Toast.makeText(this, "没有了", Toast.LENGTH_SHORT).show();
             return;
         }
 
         list.add(new Model("加载更多中...", BaseRecyclerAdapter.FOOTER_TYPE));
         adapter.notifyDataSetChanged();
+
+        //锁住再次加载的bug
+        isLoading = true;
 
         handler.postDelayed(new Runnable() {
             @Override
@@ -103,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
                 //把那个加载更多的最后一个item移除掉
                 list.remove(list.size() - 1);
 
-                for (int i = 0; i < 3; i++) {
+                for (int i = 0; i < 5; i++) {
                     list.add(new Model("普通数据:" + i, BaseRecyclerAdapter.NORMAL_TYPE));
                 }
                 if (currentPageIndex == maxPageTotal) {
@@ -111,6 +139,9 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 adapter.notifyDataSetChanged();
+
+                //释放可以触发再次加载的锁
+                isLoading = false;
             }
         }, 2000);
     }
